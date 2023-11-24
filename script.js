8d1186cd1b83115f5da99ef1d033d0cb93b9c738
leonardo <leonardo_trifiletti@yahoo.it>
2023-11-24 20:10:56 +0100

    es 2 done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Crea una pagina HTML in cui, dopo aver richiesto dei post all’API, vengono cre
 
 // Contatti con il server
 
-const prova = document.getElementById("prova");
+/*const prova = document.getElementById("prova");
 
 window.addEventListener("load", async function () {
   try {
@@ -29,5 +29,51 @@ const creazionePostCards = (post) => {
   p.innerText = post.body;
   div.appendChild(h3);
   div.appendChild(p);
+  return div;
+};*/
+
+/*# Esercizio 2 - Barzellette
+Studiati come funzionano le API di https://v2.jokeapi.dev/.
+Crea una pagina HTML in cui l’utente può decidere quante barzellette visualizzare. Dopo che l’utente ha deciso e dato conferma d’invio (con un bottone), 
+interroga l’API e crea una lista di barzellette che contiene tante barzellette (a tema Programming) quante ne ha richieste l’utente.*/
+
+const h3 = document.querySelector("h3");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+window.addEventListener("load", function () {
+  button.addEventListener("click", async function () {
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    const quantita = parseInt(input.value);
+    try {
+      const response = await fetch(
+        `https://v2.jokeapi.dev/joke/Programming?amount=${quantita}&type=single`
+      );
+
+      const { jokes } = await response.json();
+
+      jokes.forEach((joke) => {
+        const gioco = creazioneBarzellette(joke);
+        div.appendChild(gioco);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
+
+//Creazione lista di barzellette
+
+const creazioneBarzellette = (barzelletta) => {
+  const div = document.createElement("div");
+  const li = document.createElement("li");
+  if (barzelletta.setup && barzelletta.delivery) {
+    li.innerText = `${barzelletta.setup} ${barzelletta.delivery}`;
+  } else if (barzelletta.joke) {
+    li.innerText = barzelletta.joke;
+  } else {
+    li.innerText = "Barzelletta errore";
+  }
+  div.appendChild(li);
   return div;
 };
